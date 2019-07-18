aac1577eb16c6105ea2513d32f17eb314ddaf0ed
ruilip <735061202@qq.com>
2019-07-18 18:06:09 +0800

    echarts使用cdn

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import store from './store'
+// import store from './store'
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
 import './components' // 全局组件自动注册
